7209526f21f9b6627e62b1d0d4d352483a11f3eb
Evandro Dutra <evandro@jobscore.com>
2014-05-17 09:27:51 -0300

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionHub::Application.config.secret_token = '23cea55c8da0ae688da860a9114f8dc67424386bb646ae82ae51151fa1e63bcfe84eb30492d81b3f512c044f9b03ecdd29abb7afaddae58e84ee7cb239f79253'
