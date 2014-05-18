303fbb1711608dd7bdb887b3195d05cb4eb19bc0
Evandro Dutra <evandro@jobscore.com>
2014-05-18 05:33:10 -0300

    respostas

--- app/controllers/prova_controller.rb
+++ app/controllers/prova_controller.rb
@@ -1,18 +1,6 @@
 class ProvaController < ApplicationController
 
   def show
-
-    @questoes = []
-    url = URI.parse("http://ec2-54-207-247-209.sa-east-1.compute.amazonaws.com")
-    res = Net::HTTP.start(url.host, url.port) do |http|
-      http.get("/perguntas/questoesarea.php?idarea=#{params[:id]}")
-    end
-
-
-    @questoes = JSON.parse res.body[1..(res.body.length-3)].encode("iso-8859-1").force_encoding("utf-8")
-    @questoes_ids = @questoes.map{|q| {id: q['id'], idarea: q['idarea'], enunciado: q['enunciado']}}
-
-
   end
 
   def questao
