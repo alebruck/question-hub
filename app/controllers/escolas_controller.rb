9fac4fa14a2e4e13eb5e547555f9416e98939a24
Alessandro Diogo Bruckheimer <alebruck@gmail.com>
2014-05-18 02:54:44 -0300

    add busca escola

--- app/controllers/escolas_controller.rb
+++ app/controllers/escolas_controller.rb
@@ -4,4 +4,12 @@ class EscolasController < ApplicationController
   def show
     @escola = Escola.where(:pk_cod_entidade => params[:id]).first
   end
+
+  def index
+    if params[:nome].to_s.present?
+      @escolas =Escola.where(["nome like ?", "%#{params[:nome].upcase}%"]).order("nome ASC").paginate(:page => params[:page], :per_page => 30)
+    else
+      @escolas =Escola.order("nome ASC").paginate(:page => params[:page], :per_page => 30)
+    end
+  end
 end
