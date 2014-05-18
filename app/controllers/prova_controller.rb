841025759fc67bba322b3c5facef340457b0a15c
Evandro Dutra <evandro@jobscore.com>
2014-05-18 04:12:35 -0300

    pagina de questoes

--- app/controllers/prova_controller.rb
+++ app/controllers/prova_controller.rb
@@ -7,7 +7,10 @@ class ProvaController < ApplicationController
     res = Net::HTTP.start(url.host, url.port) do |http|
       http.get("/perguntas/questoesarea.php?idarea=#{params[:id]}")
     end
-    @questoes = JSON.parse res.body
+
+
+    @questoes = JSON.parse res.body[1..(res.body.length-3)].encode("iso-8859-1").force_encoding("utf-8")
+    @questoes_ids = @questoes.map{|q| {id: q['id'], idarea: q['idarea'], enunciado: q['enunciado']}}
 
 
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# -*- encoding : utf-8 -*-
 module ApplicationHelper
+
+  def tipo_descricao(desc)
+    case desc.to_i
+      when 1
+        return 'Ciências da Natureza'
+      when 2
+        return 'Ciências Humanas'
+      when 3
+        return 'Linguagens e Códigos'
+      when 4
+        return 'Matemática'
+    end
+  end
 end
